808a340e437b42ed4369c8f28c893137f49166db
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2023-08-29 13:53:24 +0300

    Update Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+    name: "PurchaseConnector-Dynamic",
+    products: [
+        .library(name: "PurchaseConnector-Dynamic", targets: ["PurchaseConnector"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "PurchaseConnector",
+            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-purchase-connector/releases/download/6.10.1/purchase-connector-dynamic.xcframework.zip",
+            checksum: "2cefdac864917b83af66050f7d336ee6b0360b87d41169fe293e99310fd32a5b"
+        ),
+    ]
+)
