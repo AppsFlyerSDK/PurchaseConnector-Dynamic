f1a93ff109ea94928516e95ace7cc40ebd80d3a3
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2024-04-24 17:15:17 +0300

    Update dependency to v6.14.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PurchaseConnector",
-            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-purchase-connector/releases/download/6.13.1/purchase-connector-dynamic.xcframework.zip",
-            checksum: "6342e83b54db7768e95c418846add16d58d7faf7c21e344364291f5014f5a7f1"
+            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-purchase-connector/releases/download/6.14.0/purchase-connector-dynamic.xcframework.zip",
+            checksum: "1976be9f5912c6a28d8a3e9ba67df40d09a99953c6b6641551ffb4df0a6ccad3"
         ),
     ]
 )
