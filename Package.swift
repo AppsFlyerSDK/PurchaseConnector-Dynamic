cbd81a34a458261875b0f786e92b98e3a35eaf9c
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2024-04-30 17:35:49 +0300

    Update PC to support AppsFlyerLib v6.14.3

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PurchaseConnector",
-            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-purchase-connector/releases/download/6.14.0/purchase-connector-dynamic.xcframework.zip",
-            checksum: "1976be9f5912c6a28d8a3e9ba67df40d09a99953c6b6641551ffb4df0a6ccad3"
+            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-purchase-connector/releases/download/6.14.3/purchase-connector-dynamic.xcframework.zip",
+            checksum: "c7f579ef9be979b03d1cc6ef32c40f843f5e147e940d7b3e1f99d21dc11f41bd"
         ),
     ]
 )
