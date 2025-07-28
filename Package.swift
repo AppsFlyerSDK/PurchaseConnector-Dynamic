d6161206b3e75946a91699cdc9a0fad76b82c7ae
Amit Levy <118281047+al-af@users.noreply.github.com>
2025-07-28 12:53:30 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PurchaseConnector",
-            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-purchase-connector/releases/tag/6.17.2/purchase-connector-dynamic.xcframework.zip",
+            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-purchase-connector/releases/download/6.17.2/purchase-connector-dynamic.xcframework.zip",
             checksum: "a244ec9aa1d7faf9e4f368ef14a8a8731b00fb3eaddced21133a1dcb960e6a9c"
         ),
     ]
