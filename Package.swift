11d0404fc20e2eda385da663dcc61e1754dc5706
Amit Levy <amit.levy@appsflyer.com>
2026-05-27 12:16:45 +0300

    chore: bump to 7.0.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PurchaseConnector",
-            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-purchase-connector/releases/download/6.18.2/purchase-connector-dynamic.xcframework.zip",
-            checksum: "adde2f018a0e9e8f1fdd6d71e641757e72d1df7c263533390e15b58cfdd1ecd3"
+            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-purchase-connector/releases/download/7.0.0/purchase-connector-dynamic.xcframework.zip",
+            checksum: "e40aa8d0e4173a564cbae9438a6e175e694c0faa767ed59321ee5660de38ed9a"
         ),
     ]
 )
